b967b878c16c56ab7476f90fb05d25e3542184a7
amesar <amesar@users.noreply.github.co>
2018-10-20 18:09:23 +0000

    mlflow-java: Updated RunContext

---
+++ mlflow-java/README.md
@@ -0,0 +1,9 @@
+# mlflow-java
+
+MLflow Java and Scala extras
+
+### RunContext Fluent Proposal
+
+* [RunContext.java](src/main/java/org/mlflow/tracking/RunContext.java)
+* [QuickStartFluent.scala](src/main/scala/org/mlflow/tracking/examples/QuickStartFluent.scala) - Sample run with RunContext
+
--- mlflow-java/src/main/java/org/mlflow/tracking/RunContext.java
+++ mlflow-java/src/main/java/org/mlflow/tracking/RunContext.java
@@ -1,36 +1,60 @@
 package org.mlflow.tracking;
 
-import org.apache.log4j.Logger;
+import java.io.File;
 import org.mlflow.api.proto.Service.RunInfo;
 import org.mlflow.api.proto.Service.RunStatus;
+import org.mlflow.api.proto.Service.CreateRun;
 
 public class RunContext implements AutoCloseable {
-    private static final Logger logger = Logger.getLogger(RunContext.class);
     private MlflowClient mlflowClient;
     private String runId;
 
-    public RunContext(MlflowClient mlflowClient, long experimentId, String sourceName) 
-        throws Exception {
+    public RunContext(MlflowClient mlflowClient) {
         this.mlflowClient = mlflowClient;
-        RunInfo runInfo = mlflowClient.createRun(experimentId, sourceName);
-        this.runId = runInfo.getRunUuid();
-        logger.debug("runId="+runId);
+        this.runId = mlflowClient.createRun().getRunUuid();
     }
 
-    public void logParam(String key, String value) throws Exception {
+    public RunContext(MlflowClient mlflowClient, long experimentId) {
+        this.mlflowClient = mlflowClient;
+        this.runId = mlflowClient.createRun(experimentId).getRunUuid();
+    }
+
+    public RunContext(MlflowClient mlflowClient, long experimentId, String sourceName) {
+        this.mlflowClient = mlflowClient;
+        this.runId = mlflowClient.createRun(experimentId, sourceName).getRunUuid();
+    }
+
+    public RunContext(MlflowClient mlflowClient, CreateRun request) {
+        this.mlflowClient = mlflowClient;
+        this.runId = mlflowClient.createRun(request).getRunUuid();
+    }
+
+    public void logParam(String key, String value) {
         mlflowClient.logParam(runId, key, value);
     }
 
-    public void logMetric(String key, float value) throws Exception {
+    public void logMetric(String key, float value) {
         mlflowClient.logMetric(runId, key, value);
     }
 
-    public void logArtifact(String localFile, String artifactPath) throws Exception {
-        throw new UnsupportedOperationException();
+    public void setTag(String key, String value) {
+        mlflowClient.setTag(runId, key, value);
     }
 
-    public void logModel(String key) throws Exception {
-        throw new UnsupportedOperationException("logModel() coming soon");
+    public void logArtifact(File localFile) {
+        mlflowClient.logArtifact(runId, localFile);
+    }
+
+    public void logArtifact(File localFile, String artifactPath) {
+        mlflowClient.logArtifact(runId, localFile, artifactPath);
+    }
+
+    public void logArtifacts(File localDir) {
+        mlflowClient.logArtifacts(runId, localDir);
+    }
+
+    public void logArtifacts(File localDir, String artifactPath) {
+        mlflowClient.logArtifacts(runId, localDir, artifactPath);
     }
 
     public String getRunId() {
@@ -38,7 +62,7 @@ public class RunContext implements AutoCloseable {
     }
 
     @Override
-    public void close() throws Exception {
+    public void close() {
         mlflowClient.setTerminated(runId, RunStatus.FINISHED, System.currentTimeMillis());
     }
 }
