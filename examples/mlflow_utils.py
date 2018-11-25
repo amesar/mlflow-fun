3fcc1ce53fdceeb916d8d65b8ea6b652a9717e47
amesar <amesar@users.noreply.github.co>
2018-11-25 17:47:17 +0000

    added get_first_run() to mlflow_utils.py

--- examples/mlflow_utils.py
+++ examples/mlflow_utils.py
@@ -13,3 +13,11 @@ def get_or_create_experiment_id(experiment_name):
         experiment_id = exp.experiment_id
     print("experiment_name={} experiment_id={}".format(experiment_name,experiment_id))
     return experiment_id
+
+def get_first_run(experiment_name):
+    tracking_uri = mlflow.tracking.get_tracking_uri()
+    client = mlflow.tracking.MlflowClient(tracking_uri)
+    exp = client.get_experiment_by_name(exp_name)
+    runs = client.list_run_infos(exp.experiment_id)
+    return runs[0]
+
