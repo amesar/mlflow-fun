1dad486f2530dd80a4027fb53f0e2e4b85ba1b48
amesar <amesar@users.noreply.github.co>
2018-11-25 17:51:58 +0000

    updated hello_world_nested_runs.py

--- examples/hello_world/hello_world_nested_runs.py
+++ examples/hello_world/hello_world_nested_runs.py
@@ -6,20 +6,17 @@ name = 'HelloWorld Nested Runs'
 
 with mlflow.start_run(experiment_id=experiment_id, run_name=name) as run:
     print("runId:",run.info.run_uuid," - name:",name)
-    print("artifact_uri:",mlflow.get_artifact_uri())
-    mlflow.set_tag("algo", name + " root")
+    mlflow.set_tag("algo", name)
     with open("info.txt", "w") as f:
-        f.write("Hi root artifact")
+        f.write(name)
     mlflow.log_artifact("info.txt")
-    
-    for i in range(0, 2):
-        irun = str(i)
-        name2 = name + " " + irun
+    for j in range(0, 2):
+        name2 = name + " " + str(j)
         with mlflow.start_run(run_name=name2, nested=True) as run2:
             print("runId.2:",run2.info.run_uuid," - name2:",name2)
-            mlflow.log_param("alpha", str(i+0.1))
-            mlflow.log_metric("auroch", i+0.123)
-            mlflow.set_tag("algo", "hello_world_"+irun)
+            mlflow.log_param("alpha", str(j+0.1))
+            mlflow.log_metric("auroch", j+0.123)
+            mlflow.set_tag("algo", name2)
             with open("info.txt", "w") as f:
-                f.write("Hi artifact run "+irun)
+                f.write(name2)
             mlflow.log_artifact("info.txt")
