918debb566ee24596c4d8fe070b68a8e9a98fe6b
amesar <amesar@users.noreply.github.co>
2018-12-04 18:45:32 +0000

    hello_world: changed param tag to run_origin

--- examples/hello_world/hello_world.py
+++ examples/hello_world/hello_world.py
@@ -2,16 +2,16 @@ from __future__ import print_function
 import sys
 import mlflow
 
-def run(alpha, tag, log_artifact):
-    with mlflow.start_run(run_name="Hello World") as run:
+def run(alpha, run_origin, log_artifact):
+    with mlflow.start_run(run_name=run_origin) as run:
         print("runId:",run.info.run_uuid)
         print("artifact_uri:",mlflow.get_artifact_uri())
         print("alpha:",alpha)
-        print("tag:",tag)
         print("log_artifact:",log_artifact)
+        print("run_origin:",run_origin)
         mlflow.log_param("alpha", alpha)
         mlflow.log_metric("auroch", 0.123)
-        mlflow.set_tag("origin", tag)
+        mlflow.set_tag("run_origin", run_origin)
         mlflow.set_tag("log_artifact", log_artifact)
         if log_artifact:
             with open("info.txt", "w") as f:
@@ -20,6 +20,6 @@ def run(alpha, tag, log_artifact):
 
 if __name__ == "__main__":
     alpha = sys.argv[1] if len(sys.argv) > 1 else "0.1"
-    tag = sys.argv[2] if len(sys.argv) > 2 else "None"
+    run_origin = sys.argv[2] if len(sys.argv) > 2 else "None"
     log_artifact = bool(sys.argv[3]) if len(sys.argv) > 3 else False
-    run(alpha,tag,log_artifact)
+    run(alpha,run_origin,log_artifact)
