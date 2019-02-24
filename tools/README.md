2a8f503d8649df7e8c4369206a24aa1aa210ebb1
amesar <amesar@users.noreply.github.co>
2019-02-24 20:11:54 +0000

    tools/export_import: added log_source_info for import metadata tags

--- tools/README.md
+++ tools/README.md
@@ -75,14 +75,27 @@ Created experiment 'local_runs' with id 1
 
 **Export and import the run**
 
-Then run [export_import.py](export_import.py). The dst_experiment_id option refers to the destination experiment ID.
+Then run [export_import_run.py](export_import_run.py). 
+
 ```
 export MLFLOW_TRACKING_URI=http://localhost:5000
 
-python export_import.py \
+python export_import_run.py \
   --src_run_id 50fa90e751eb4b3f9ba9cef0efe8ea30 \
   --dst_experiment_id 123 \
   --dst_uri http://localhost:5001
+  --log_source_info
+```
+
+The log_source_info creates metadata tags with import information. Sample tags from UI:
+```
+Name                         Value
+_exim_DATABRICKS_HOST        https://myshard.cloud.databricks.com
+_exim_import_timestamp       1551037752
+_exim_import_timestamp_nice  2019-02-24 19:49:12
+_exim_src_experiment_id      2368826
+_exim_src_run_id             d9bbe1c8c94f434b8514301a0834063c
+_exim_src_uri                databricks
 ```
 
 **Check predictions from new run**
--- tools/export_import_run.py
+++ tools/export_import_run.py
@@ -1,4 +1,3 @@
-
 """ 
 Exports a run from one MLflow server and imports it into another server. 
 First create an experiment in the destination MLflow server: mlflow experiments create my_experiment
@@ -7,17 +6,21 @@ First create an experiment in the destination MLflow server: mlflow experiments 
 from __future__ import print_function
 
 import os
+import time
+import datetime
 import shutil
 from argparse import ArgumentParser
 import mlflow
 from mlflow import tracking
 
-def run(src_run_id, dst_exp_id, dst_uri):
+def run(src_run_id, dst_exp_id, dst_uri, log_source_info):
     print("src_run_id:",src_run_id)
     print("dst_exp_id:",dst_exp_id)
     print("dst_uri:",dst_uri)
+    print("log_source_info:",log_source_info)
 
     src_client = mlflow.tracking.MlflowClient()
+    src_uri = mlflow.tracking.get_tracking_uri()
     src_run = src_client.get_run(src_run_id)
 
     dst_client = mlflow.tracking.MlflowClient(dst_uri)
@@ -43,6 +46,18 @@ def run(src_run_id, dst_exp_id, dst_uri):
         for e in src_run.data.tags:
              mlflow.set_tag(e.key, e.value)
 
+        if log_source_info:
+             mlflow.set_tag("_exim_src_run_id",src_run_id)
+             mlflow.set_tag("_exim_src_experiment_id",src_run.info.experiment_id)
+             mlflow.set_tag("_exim_src_uri",src_uri)
+             dbx_host = os.environ.get("DATABRICKS_HOST",None)
+             if dbx_host is not None:
+                 mlflow.set_tag("_exim_DATABRICKS_HOST", dbx_host)
+             now = int(time.time()+.5)
+             snow = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(now))
+             mlflow.set_tag("_exim_import_timestamp",now)
+             mlflow.set_tag("_exim_import_timestamp_nice",snow)
+
         # copy artifacts
         local_path = src_client.download_artifacts(src_run_id,"")
         print("local_path:",local_path)
@@ -52,8 +67,8 @@ def run(src_run_id, dst_exp_id, dst_uri):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--src_run_id", dest="src_run_id", help="Source run_id", required=True)
-    parser.add_argument("--dst_uri", dest="dst_uri", help="Destination API URL", required=True)
+    parser.add_argument("--dst_uri", dest="dst_uri", help="Destination MLFLOW API URL", required=True)
     parser.add_argument("--dst_experiment_id", dest="dst_experiment_id", help="Destination experiment_id", required=True, type=int)
+    parser.add_argument("--log_source_info", dest="log_source_info", help="Set tags with import information", default=False, action='store_true')
     args = parser.parse_args()
-    run(args.src_run_id, args.dst_experiment_id, args.dst_uri)
-
+    run(args.src_run_id, args.dst_experiment_id, args.dst_uri, args.log_source_info)
