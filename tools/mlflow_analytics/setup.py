643aaccbb3020a4397f91f41800d16f1356e76c4
amesar <amesar@users.noreply.github.co>
2019-03-03 22:50:22 +0000

    tools/mlflow_analytics: added setup.py

---
+++ tools/mlflow_analytics/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mlflow_analytics',
+      version='0.0.1',
+      description='Create Spark tables from MLflow experiments and runs',
+      author='Andre',
+      packages=['mlflow_analytics'],
+      zip_safe=False)
