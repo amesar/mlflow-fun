6870b11b3a804ca69e0d257064dc84d3199c6f8b
amesar <amesar@users.noreply.github.co>
2019-03-18 20:37:05 +0000

    tools_pkg: setup.py add

---
+++ tools/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mlflow_fun',
+      version='0.0.1',
+      description='MLflowFun tools',
+      author='Andre',
+      packages=['mlflow_fun',
+                'mlflow_fun.common',
+                'mlflow_fun.analytics'],
+      zip_safe=False)
