fbe7605c652ac3bbf3e51be4692c907428c3ce79
amesar <amesar@users.noreply.github.co>
2019-04-29 21:20:26 +0000

    mlflow_fun/metrics: updated api_best.py with new search() python method per 0.9.1

--- tools/mlflow_fun/metrics/api/api_best.py
+++ tools/mlflow_fun/metrics/api/api_best.py
@@ -1,40 +1,51 @@
-
-import os
-import json
-import requests
+from argparse import ArgumentParser
 import mlflow
+client = mlflow.tracking.MlflowClient()
 
 def lt(x,y): return x < y
 def gt(x,y): return x > y
 
-def get_best_run_slow(experiment_id, metric, ascending=False):
-    funk = lt if ascending else gt
-    client = mlflow.tracking.MlflowClient()
-    infos = client.list_run_infos(experiment_id)
-    best = None
-    for info in infos:
-        run = client.get_run(info.run_uuid)
-        for m in run.data.metrics:
-            if m.key == metric:
-               if best is None or funk(m.value,best[1]):
-                   best = (info.run_uuid,m.value)
+def calc(metric,run, best, funk):
+    for m in run.data.metrics:
+        if m.key == metric and (best is None or funk(m.value,best[1])):
+           best = (run.info.run_uuid,m.value)
     return best
 
-def get_best_run_fast(host, token, experiment_id, metric, ascending=False):
+"""
+Finds the best run by calling get_run for each run. 
+"""
+def get_best_run_slow(experiment_id, metric, ascending=False):
     funk = lt if ascending else gt
-    api_path = "api/2.0/preview/mlflow"
-    uri = os.path.join(host,api_path,"runs/search")
-    req = '{ "experiment_ids": ['+str(experiment_id)+'] }'
-    headers = {} if token is None else {'Authorization': 'Bearer '+token}
-    rsp = requests.get(uri, data=req, headers=headers)
-    runs = json.loads(rsp.text)['runs']
     best = None
-    for run in runs:
-        if 'data' in run and 'metrics' in run['data']:
-            mlist = run['data']['metrics']
-            mdct = { x['key'] : x['value'] for x in mlist }
-            if metric in mdct:
-               mval = mdct[metric]
-               if best is None or (isinstance(mval,float) and funk(mval,best[1])):
-                  best = (run['info']['run_uuid'],mval)     
+    infos = client.list_run_infos(experiment_id)
+    for info in infos:
+        run = client.get_run(info.run_uuid)
+        best = calc(metric,run, best, funk)
     return best
+
+"""
+Finds the best run by calling search once to get data for all an experiment's runs
+"""
+def get_best_run_fast(experiment_id, metric, ascending=False):
+    funk = lt if ascending else gt
+    best = None
+    runs = client.search_runs([experiment_id],"")
+    for run in runs:
+        best = calc(metric,run, best, funk)
+    return best
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--experiment_id", dest="experiment_id", help="Experiment ID", type=str, required=True)
+    parser.add_argument("--metric", dest="metric", help="Metric", type=str, required=True)
+    parser.add_argument("--ascending", dest="ascending", help="ascending", required=False, default=False, action="store_true")
+    parser.add_argument("--which", dest="which", help="Which: fast|slow|both", type=str, default="both")
+    args = parser.parse_args()
+    
+    if args.which in ['slow','both']:
+        best = get_best_run_fast(args.experiment_id, args.metric, args.ascending)
+        print("fast best:",best)
+    if args.which in ['fast','both']:
+        best = get_best_run_slow(args.experiment_id, args.metric, args.ascending)
+        print("slow best:",best)
+        
--- tools/mlflow_fun/metrics/api/main_api.py
+++ tools/mlflow_fun/metrics/api/main_api.py
@@ -3,15 +3,15 @@ from mlflow_fun.metrics.api.api_best import *
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--host", dest="host", help="host", required=True)
-    parser.add_argument("--token", dest="token", help="token", required=False, default=None)
-    parser.add_argument("--experiment_id", dest="experiment_id", help="Experiment ID", type=int, required=True)
+    parser.add_argument("--experiment_id", dest="experiment_id", help="Experiment ID", type=str, required=True)
     parser.add_argument("--metric", dest="metric", help="Metric", type=str, required=True)
-    parser.add_argument("--ascending", dest="ascending", help="ascending", required=False, default=False, action='store_true')
+    parser.add_argument("--ascending", dest="ascending", help="ascending", required=False, default=False, action="store_true")
+    parser.add_argument("--which", dest="which", help="Which: fast|slow|both", type=str, default="both")
     args = parser.parse_args()
 
-    best = get_best_run_slow(args.experiment_id, args.metric, args.ascending)
-    print("slow best:",best)
-
-    best = get_best_run_fast(args.host, args.token, args.experiment_id, args.metric, args.ascending)
-    print("fast best:",best)
+    if args.which in ['slow','both']:
+        best = get_best_run_fast(args.experiment_id, args.metric, args.ascending)
+        print("fast best:",best)
+    if args.which in ['fast','both']:
+        best = get_best_run_slow(args.experiment_id, args.metric, args.ascending)
+        print("slow best:",best)
