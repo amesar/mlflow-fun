078ec1b38b3aa10ecc8ee325911d1baf8fca1de2
amesar <amesar@users.noreply.github.co>
2019-05-10 17:01:18 +0000

    examples/scikit-learn: main.py - made experiment-name optional

--- examples/scikit-learn/wine-quality/main.py
+++ examples/scikit-learn/wine-quality/main.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--experiment_name", dest="experiment_name", help="experiment_name", required=True)
+    parser.add_argument("--experiment_name", dest="experiment_name", help="experiment_name", default="sklearn", required=False)
     parser.add_argument("--data_path", dest="data_path", help="data_path", required=True)
     parser.add_argument("--alpha", dest="alpha", help="alpha", default=0.1, type=float )
     parser.add_argument("--l1_ratio", dest="l1_ratio", help="l1_ratio", default=0.1, type=float )
