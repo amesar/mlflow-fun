b052c7c72986024e65eee54bd1bcbab152bdb4a0
amesar <amesar@users.noreply.github.co>
2019-06-15 15:52:39 +0000

    tools/mlflow_fun/metrics/api: fixed 0.9.1 issue in api_best.py

--- tools/mlflow_fun/metrics/api/api_best.py
+++ tools/mlflow_fun/metrics/api/api_best.py
@@ -7,8 +7,8 @@ def gt(x,y): return x > y
 
 def calc(metric_name, run, best, funk):
     metric_value = run.data.metrics.get(metric_name,None)
-    if metric_value is not None and (best is None or funk(m.value,best[1])):
-         best = (run.info.run_uuid,metric_value)
+    if metric_value is not None and (best is None or funk(metric_value,best[1])):
+         best = (run.info.run_uuid, metric_value)
     return best
 
 """
@@ -20,7 +20,7 @@ def get_best_run_slow(experiment_id, metric, ascending=False):
     infos = client.list_run_infos(experiment_id)
     for info in infos:
         run = client.get_run(info.run_uuid)
-        best = calc(metric,run, best, funk)
+        best = calc(metric, run, best, funk)
     return best
 
 """
@@ -31,7 +31,7 @@ def get_best_run_fast(experiment_id, metric, ascending=False):
     best = None
     runs = client.search_runs([experiment_id],"")
     for run in runs:
-        best = calc(metric,run, best, funk)
+        best = calc(metric, run, best, funk)
     return best
 
 if __name__ == "__main__":
@@ -41,11 +41,11 @@ if __name__ == "__main__":
     parser.add_argument("--ascending", dest="ascending", help="ascending", required=False, default=False, action="store_true")
     parser.add_argument("--which", dest="which", help="Which: fast|slow|both", type=str, default="both")
     args = parser.parse_args()
+    print("args:",args)
     
     if args.which in ['slow','both']:
-        best = get_best_run_fast(args.experiment_id, args.metric, args.ascending)
-        print("fast best:",best)
-    if args.which in ['fast','both']:
         best = get_best_run_slow(args.experiment_id, args.metric, args.ascending)
         print("slow best:",best)
-        
+    if args.which in ['fast','both']:
+        best = get_best_run_fast(args.experiment_id, args.metric, args.ascending)
+        print("fast best:",best)
