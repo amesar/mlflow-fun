cd1164d7f892db6a850057e834be5e4083bbd95d
amesar <amesar@users.noreply.github.co>
2019-06-28 20:10:13 +0000

    tools/mlflow_fun/tools: rationalization of dump scripts

--- tools/mlflow_fun/tools/dump_experiment.py
+++ tools/mlflow_fun/tools/dump_experiment.py
@@ -4,51 +4,51 @@ Recursively dumps all information about an experiment including all details of i
 Note that this can be expensive. Adjust your artifact_max_level.
 """
 
-from __future__ import print_function
-import sys
-for x in sys.path: print("  path:",x)
-from argparse import ArgumentParser
 import mlflow
 from mlflow_fun.tools import dump_run
 
+client = mlflow.tracking.MlflowClient()
 print("MLflow Version:", mlflow.version.VERSION)
 
-def dump_experiment(exp):
-    print("Experiment Details:")
-    for k,v in exp.__dict__.items(): print("  {}: {}".format(k[1:],v))
-  
-def get_runs(client, infos, artifact_max_level):
-    for info in infos:
-        dump_run.dump_run_id_with_artifacts(info.run_uuid, artifact_max_level)
-
-def dump_experiment_with_runs(exp_id_or_name, artifact_max_level, show_runs):
+def dump_experiment(exp_id_or_name, artifact_max_level, show_runs):
     print("Options:")
     print("  exp_id_or_name:",exp_id_or_name)
-    print("  exp_id_or_name.type:",type(exp_id_or_name))
     print("  artifact_max_level:",artifact_max_level)
     print("  show_runs:",show_runs)
 
-    client = mlflow.tracking.MlflowClient()
     if exp_id_or_name.isdigit():
-        exp_id = exp_id_or_name
+        exp = client.get_experiment(exp_id_or_name)
+        which = "ID"
     else:
-        print("experiment_name:",exp_id_or_name)
-        exp_id = client.get_experiment_by_name(exp_id_or_name).experiment_id
+        exp = client.get_experiment_by_name(exp_id_or_name)
+        which = "name"
+    if exp is None:
+         raise Exception("Cannot find experiment {} '{}'".format(which,exp_id_or_name))
+    exp_id = exp.experiment_id
     print("experiment_id:",exp_id)
-    exp = client.get_experiment(exp_id)
-    dump_experiment(exp)
-    infos = client.list_run_infos(exp_id)
-    print("  #runs:",len(infos))
-    if not show_runs: 
-        return
+    dump_experiment_details(exp)
 
-    get_runs(client,infos,artifact_max_level)
-    print("#runs:",len(infos))
+    if show_runs: 
+        infos = client.list_run_infos(exp_id)
+        print("  #runs:",len(infos))
+        dump_runs(infos,artifact_max_level)
+
+def dump_experiment_details(exp):
+    print("Experiment Details:")
+    for k,v in exp.__dict__.items(): print("  {}: {}".format(k[1:],v))
+  
+def dump_runs(infos, artifact_max_level):
+    print("Runs:")
+    for j,info in enumerate(infos):
+        print("  Run {}:".format(j))
+        dump_run.dump_run_id(info.run_uuid, artifact_max_level, indent="    ")
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument("--experiment_id_or_name", dest="experiment_id", help="Experiment ID", required=True)
     parser.add_argument("--artifact_max_level", dest="artifact_max_level", help="Number of artifact levels to recurse", required=False, default=1, type=int)
     parser.add_argument("--show_runs", dest="show_runs", help="Show runs", required=False, default=False, action='store_true')
     args = parser.parse_args()
-    dump_experiment_with_runs(args.experiment_id, args.artifact_max_level,args.show_runs)
+    print("args:",args)
+    dump_experiment(args.experiment_id, args.artifact_max_level,args.show_runs)
--- tools/mlflow_fun/tools/dump_run.py
+++ tools/mlflow_fun/tools/dump_run.py
@@ -2,17 +2,35 @@
 Run dump utilities.
 """
 
-from __future__ import print_function
 import time
-from argparse import ArgumentParser
 import mlflow
 
 INDENT = "  "
 MAX_LEVEL = 1
 TS_FORMAT = "%Y-%m-%d_%H:%M:%S"
+client = mlflow.tracking.MlflowClient()
 
-def dump_run_info(info, name="RunInfo", indent=""):
-    print("{}:".format(name))
+def dump_run(run, max_level=1, indent=""):
+    dump_run_info(run.info,indent)
+    print(indent+"Params:")
+    for k,v in sorted(run.data.params.items()):
+        print(indent+"  {}: {}".format(k,v))
+    print(indent+"Metrics:")
+    for k,v in sorted(run.data.metrics.items()):
+        print(indent+"  {}: {}".format(k,v))
+    print(indent+"Tags:")
+    for k,v in sorted(run.data.tags.items()):
+        print(indent+"  {}: {}".format(k,v))
+    print("{}Artifacts:".format(indent))
+    dump_artifacts(run.info.run_id, "", 0, max_level, indent+INDENT)
+    return run
+        
+def dump_run_id(run_id, max_level=1, indent=""):
+    run = client.get_run(run_id)
+    return dump_run(run,max_level,indent)
+
+def dump_run_info(info, indent=""):
+    print("{}RunInfo:".format(indent))
     for k,v in sorted(info.__dict__.items()):
         if not k.endswith("_time"):
             print("{}  {}: {}".format(indent,k[1:],v))
@@ -21,25 +39,6 @@ def dump_run_info(info, name="RunInfo", indent=""):
     if start is not None and end is not None:
         dur = float(end - start)/1000
         print("{}  _duration:  {} seconds".format(indent,dur))
-
-def dump_run(run):
-    print("Run")
-    dump_run_info(run.info,"  Info","  ")
-    print("  Params:")
-    for k,v in sorted(run.data.params.items()):
-        print("    {}: {}".format(k,v))
-    print("  Metrics:")
-    for k,v in sorted(run.data.metrics.items()):
-        print("    {}: {}".format(k,v))
-    print("  Tags:")
-    for k,v in sorted(run.data.tags.items()):
-        print("    {}: {}".format(k,v))
-    return run
-        
-def dump_run_id(run_id, client=None):
-    if client is None: client = mlflow.tracking.MlflowClient()
-    run = client.get_run(run_id)
-    return dump_run(run)
 
 def _dump_time(info, k, indent=""):
     v = info.__dict__.get(k,None)
@@ -50,41 +49,20 @@ def _dump_time(info, k, indent=""):
         print("{}  {:<11} {}   {}".format(indent,k[1:]+":",stime,v))
     return v
 
-def dump_artifact(art, indent="", level=0):
-    print("{}Artifact - level {}:".format(indent,level))
-    for k,v in art.__dict__.items(): print("  {}{}: {}".format(indent,k[1:],v))
-
-def _dump_artifacts(client, run_id, path, indent, level, max_level):
-    level += 1
-    if level > max_level: return
+def dump_artifacts(run_id, path, level, max_level, indent):
+    if level+1 > max_level: return
     artifacts = client.list_artifacts(run_id,path)
     for art in artifacts:
-        dump_artifact(art,indent+INDENT,level)
+        print("{}Artifact - level {}:".format(indent,level))
+        for k,v in art.__dict__.items(): print("  {}{}: {}".format(indent,k[1:],v))
         if art.is_dir:
-            _dump_artifacts(client, run_id, art.path, indent+INDENT,level,max_level)
+            dump_artifacts(run_id, art.path, level+1, max_level, indent+INDENT)
 
-def dump_artifacts(client, run_id, path="", indent="", max_level=MAX_LEVEL):
-    print("{}Artifacts:".format(indent))
-    _dump_artifacts(client, run_id, path, indent, 0, max_level)
-
-
-def dump_run_id_with_artifacts(run_id, max_level=1, client=None):
-    if client is None: client = mlflow.tracking.MlflowClient()
-    dump_run_id(run_id)
-    dump_artifacts(client, run_id, path="", indent="  ", max_level=max_level)
-
-def dump_run_id_with_artifacts(run_id, max_level=1, client=None):
-    if client is None: client = mlflow.tracking.MlflowClient()
-    dump_run_id(run_id)
-    dump_artifacts(client, run_id, path="", indent="  ", max_level=max_level) # XX
-
-    
 if __name__ == "__main__":
+    from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument("--run_id", dest="run_id", help="Run ID", required=True)
     parser.add_argument("--artifact_max_level", dest="artifact_max_level", help="Number of artifact levels to recurse", required=False, default=1, type=int)
     args = parser.parse_args()
     print("args:",args)
-
-    dump_run_id_with_artifacts(args.run_id, args.artifact_max_level)
-
+    dump_run_id(args.run_id, args.artifact_max_level)
