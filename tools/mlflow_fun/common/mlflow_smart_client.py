a20a10d353be6ca0b161cbe12d63fb34b7ef1a30
amesar <amesar@users.noreply.github.co>
2019-07-21 03:49:30 +0000

    tools/common/mlflow_smart_client.py: added switch to use fast or slow list_runs variants

--- tools/mlflow_fun/common/mlflow_smart_client.py
+++ tools/mlflow_fun/common/mlflow_smart_client.py
@@ -1,14 +1,25 @@
-from __future__ import print_function
 import mlflow
-import pandas as pd
 
-''' Client with extra optimized functionality based on search_runs '''
+""" 
+Client with extra optimized functionality based on search_runs 
+"""
 class MlflowSmartClient(object):
-    def __init__(self, mlflow_client=None):
+    def __init__(self, mlflow_client=None, use_fast=True):
         if mlflow_client == None:
             mlflow_client = mlflow.tracking.MlflowClient()
         self.mlflow_client = mlflow_client
+        self.use_fast = use_fast
 
-    ''' List all run data: info, data.params, data.metrics and data.tags '''
+    """ List all run data: info, data.params, data.metrics and data.tags """
     def list_runs(self, experiment_id):
+        if self.use_fast:
+            return self.list_runs_fast(experiment_id)
+        else:
+            return self.list_runs_slow(experiment_id)
+
+    def list_runs_fast(self, experiment_id):
         return self.mlflow_client.search_runs([experiment_id], "")
+
+    def list_runs_slow(self, experiment_id):
+        infos = self.mlflow_client.list_run_infos(experiment_id)
+        return [ self.mlflow_client.get_run(info.run_id) for info in infos ]
