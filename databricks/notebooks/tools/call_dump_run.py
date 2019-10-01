aa48f09c0fab19a3fb931506b8abffad03983637
Andre Mesarovic <amesar@users.noreply.github.com>
2019-10-01 15:18:20 -0400

    add databricks/notebooks/tools/call_dump_run.py

---
+++ databricks/notebooks/tools/call_dump_run.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+run_id = "2bd3b06dcb2a4b0799535c47316af1fd"
+dbutils.notebook.run("dump_run", 60, {"Run ID": run_id})
+
+# COMMAND ----------
+
+run_id = "2bd3b06dcb2a4b0799535c47316af1fd"
+dbutils.notebook.run("dump_run", 60, {"Run ID": run_id})
