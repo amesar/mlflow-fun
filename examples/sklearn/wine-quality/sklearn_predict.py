7d9a97988df98d4fc8753f744fd834b7f7c2b1c4
amesar <amesar@users.noreply.github.co>
2019-10-21 22:12:59 +0000

    examples/sklearn/wine-quality: minor code format tweaks

--- examples/sklearn/wine-quality/sklearn_predict.py
+++ examples/sklearn/wine-quality/sklearn_predict.py
@@ -1,7 +1,6 @@
 
 # Serve predictions with mlflow.sklearn.load_model()
 
-from __future__ import print_function
 import sys
 import mlflow
 import mlflow.sklearn
@@ -16,6 +15,6 @@ if __name__ == "__main__":
     data_path = sys.argv[2] if len(sys.argv) > 2 else "../../data/wine-quality/wine-quality-red.csv"
     print("data_path:",data_path)
     print("run_id:",run_id)
-    model = mlflow.sklearn.load_model("runs:/{}/sklearn-model".format(run_id))
+    model = mlflow.sklearn.load_model(f"runs:/{run_id}/sklearn-model")
     print("model:",model)
     predict_util.run_predictions(model,data_path)
