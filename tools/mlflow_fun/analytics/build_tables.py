323a590caa289417360b0eaf49ff1c3da90cf366
amesar <amesar@users.noreply.github.co>
2019-10-21 11:46:33 +0000

    mlflow-fun/tools: minor syntax improvement

--- tools/mlflow_fun/analytics/build_tables.py
+++ tools/mlflow_fun/analytics/build_tables.py
@@ -11,6 +11,8 @@ import sys, os, time
 from argparse import ArgumentParser
 import mlflow
 from mlflow_fun.common import mlflow_utils
+from mlflow_fun.common.search_runs_iterator import SearchRunsIterator
+MAX_RESULTS = 1000
 mlflow_utils.dump_mlflow_info()
 
 mlflow_client = mlflow.tracking.MlflowClient()
@@ -37,7 +39,7 @@ class BuildTables(object):
 
     def get_values(self, obj):
         values = list(obj.__dict__.values())
-        values = [ 0 if x is None else x for x in values ] # TODO: Hack to ensure end_time stays long since a single None makes the column a string ;)
+        values = [ x or 0 for x in values ] # TODO: Hack to ensure end_time stays long since a single None makes the column a string ;)
         return values
 
     def mk_data_path(self,table):
@@ -78,11 +80,9 @@ class BuildTables(object):
         rows = []
         for j,exp in enumerate(exps):
             try:
-                #runs = mlflow_client.search_runs([exp.experiment_id],"") # does not return more than 1000 runs 
-                runs = mlflow_client.list_run_infos(exp.experiment_id)
-                print("{}/{} Experiment {} has {} runs - {}".format((1+j),len(exps),exp.experiment_id,len(runs),exp.name))
-                for info in runs:
-                    run = mlflow_client.get_run(info.run_id)
+                it = SearchRunsIterator(mlflow_client, [exp.experiment_id], MAX_RESULTS)
+                print("{}/{} Experiment {} - {}".format((1+j),len(exps),exp.experiment_id,exp.name))
+                for j,run in enumerate(it):
                     if run_row is None:
                         columns = self.get_colum_names(run.info)
                         columns += column_tags.keys()
@@ -101,6 +101,7 @@ class BuildTables(object):
                     else:
                         user_id = values[idx_user_id] 
                     rows.append(run_row(*values))
+                print("{}/{} Experiment {} has {} runs - {}".format((1+j),len(exps),exp.experiment_id,j,exp.name))
             except Exception as e:
                 print("WARNING: exp_id:",exp.experiment_id,"ex:",e)
                 import traceback
