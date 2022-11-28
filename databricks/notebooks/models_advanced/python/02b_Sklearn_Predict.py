145d58ccd9cef2f97dc17beb5dd6dbafe79c6e83
Andre Mesarovic <amesar@users.noreply.github.com>
2022-11-28 11:00:11 -0500

    Snapshot - ONNX working - 2021-07-26

--- databricks/notebooks/models_advanced/python/02b_Sklearn_Predict.py
+++ databricks/notebooks/models_advanced/python/02b_Sklearn_Predict.py
@@ -1,14 +1,26 @@
 # Databricks notebook source
 # MAGIC %md ## MLflow Advanced Sklearn Predict
 # MAGIC * Runs predictions for runs from [02a_Sklearn_Train](https://demo.cloud.databricks.com/#notebook/3528347).
-# MAGIC * Predicts in several different ways:
+# MAGIC * Predicts with following flavors:
 # MAGIC   * sklearn - [mlflow.sklearn.load_model()](https://mlflow.org/docs/latest/python_api/mlflow.sklearn.html#mlflow.sklearn.load_model)
-# MAGIC   * UDF (pyfunc) - [mlflow.pyfunc.spark_udf()](https://mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#mlflow.pyfunc.spark_udf)
-# MAGIC   * pyfunc - [mlflow.pyfunc.load_pyfunc()](https://mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#mlflow.pyfunc.load_pyfunc)
+# MAGIC   * UDF - [mlflow.pyfunc.spark_udf()](https://mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#mlflow.pyfunc.spark_udf) - as DataFrame and SQL
+# MAGIC   * pyfunc - both klearn and ONX - [mlflow.pyfunc.load_pyfunc()](https://mlflow.org/docs/latest/python_api/mlflow.pyfunc.html#mlflow.pyfunc.load_pyfunc)
+# MAGIC   * ONNX - [mlflow.onnx.load_model](https://www.mlflow.org/docs/latest/python_api/mlflow.onnx.html#mlflow.onnx.load_model)
 
 # COMMAND ----------
 
 # MAGIC %run ./predict_widgets_description.txt
+
+# COMMAND ----------
+
+# MAGIC %md ### Pip install libraries
+
+# COMMAND ----------
+
+# MAGIC %pip install onnx==1.9.0
+# MAGIC %pip install onnxmltools==1.7.0
+# MAGIC %pip install onnxruntime==1.8.0
+# MAGIC %pip install mlflow==1.19.0
 
 # COMMAND ----------
 
@@ -42,7 +54,7 @@ dump_run_id(run_id)
 
 # COMMAND ----------
 
-data_path = "dbfs:/tmp/mlflow_wine-quality.csv"
+data_path = mk_dbfs_path(download_wine_file())
 df = spark.read.option("inferSchema",True).option("header", True).csv(data_path)
 df = df.drop("quality")
 
@@ -66,12 +78,12 @@ def concat_predictions(predictions, pandas_df):
 
 # COMMAND ----------
 
-model_uri = "runs:/{}/sklearn-model".format(run_id)
+model_uri = f"runs:/{run_id}/sklearn-model"
 model_uri
 
 # COMMAND ----------
 
-# MAGIC %md #### Predict with mlflow.pyfunc.spark_udf
+# MAGIC %md #### Predict as UDF
 
 # COMMAND ----------
 
@@ -82,7 +94,14 @@ model_uri
 import mlflow.pyfunc
 
 udf = mlflow.pyfunc.spark_udf(spark, model_uri)
+
+# COMMAND ----------
+
 predictions = df.withColumn("prediction", udf(*df.columns))
+type(predictions), predictions.count(), len(predictions.columns)
+
+# COMMAND ----------
+
 display(predictions)
 
 # COMMAND ----------
@@ -101,31 +120,92 @@ df.createOrReplaceGlobalTempView("data")
 
 # COMMAND ----------
 
-# MAGIC %md #### Predict with mlflow.sklearn.load_model
+# MAGIC %md #### Predict with Sklearn flavor
 
 # COMMAND ----------
 
 import mlflow.sklearn
-
 model = mlflow.sklearn.load_model(model_uri)
+
+# COMMAND ----------
+
 pandas_df = df.toPandas()
 predictions = model.predict(pandas_df)
+type(predictions), predictions.shape
+
+# COMMAND ----------
+
 display(concat_predictions(predictions, pandas_df))
 
 # COMMAND ----------
 
-# MAGIC %md #### Predict with mlflow.pyfunc.load_pyfunc
+# MAGIC %md #### Predict as Pyfunc/Sklearn flavor
 
 # COMMAND ----------
 
-mlflow_client = mlflow.tracking.MlflowClient()
-
-model_uri = mlflow_client.get_run(run_id).info.artifact_uri + "/sklearn-model"
-model_uri = model_uri.replace("dbfs:","/dbfs")
-model = mlflow.pyfunc.load_pyfunc(model_uri)
-
+model = mlflow.pyfunc.load_model(model_uri)
 predictions = model.predict(pandas_df)
-display(concat_predictions(predictions, pandas_df))
+type(predictions), predictions.shape
+
+# COMMAND ----------
+
+#display(concat_predictions(predictions, pandas_df))
+display(pd.DataFrame(predictions, columns=["prediction"]))
+
+# COMMAND ----------
+
+# MAGIC %md ### Predict as ONNX
+
+# COMMAND ----------
+
+client = mlflow.tracking.MlflowClient()
+artifacts = client.list_artifacts(run_id, "onnx-model")
+if len(artifacts) == 0:
+    print("Exiting since there is no ONNX flavor")
+    dbutils.notebook.exit(run_id)
+
+# COMMAND ----------
+
+# MAGIC %md #### Predict as Pyfunc/ONNX flavor
+
+# COMMAND ----------
+
+model_uri = f"runs:/{run_id}/onnx-model"
+model_uri
+
+# COMMAND ----------
+
+model = mlflow.pyfunc.load_model(model_uri)
+predictions = model.predict(pandas_df)
+type(predictions), predictions.shape
+
+# COMMAND ----------
+
+display(predictions)
+
+# COMMAND ----------
+
+# MAGIC %md #### Predict as direct ONNX flavor
+
+# COMMAND ----------
+
+import mlflow.onnx
+import onnxruntime
+import numpy as np
+
+model = mlflow.onnx.load_model(model_uri)
+sess = onnxruntime.InferenceSession(model.SerializeToString())
+input_name = sess.get_inputs()[0].name
+predictions = sess.run(None, {input_name: pandas_df.to_numpy().astype(np.float32)})[0]
+type(predictions), predictions.shape
+
+# COMMAND ----------
+
+display(pd.DataFrame(predictions, columns=["prediction"]))
+
+# COMMAND ----------
+
+# MAGIC %md ## Return
 
 # COMMAND ----------
 
